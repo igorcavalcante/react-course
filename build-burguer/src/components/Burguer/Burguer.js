efe03b04f9655d4654a69a10e0c8ee399383c13f
Igor Cavalcante <igor.cavalcante@b2wdigital.com>
2018-02-02 14:57:09 -0200

    burguer controls

--- build-burguer/src/components/Burguer/Burguer.js
+++ build-burguer/src/components/Burguer/Burguer.js
@@ -4,13 +4,24 @@ import classes from './Burguer.css'
 import BurguerIngredient from './BurguerIngredients/BurguerIngredient'
 
 const burguer = (props) => {
+
+    const calcIngredients = () => {
+        const ingredients = Object.keys(props.ingredients).map(k =>
+            [...Array(props.ingredients[k])].map((_, i) => <BurguerIngredient key={i} type={k} />)
+        ).reduce((acc, value) => acc.concat(value))
+
+        if (ingredients.length == 0) {
+            return <p>Please start inserting ingredients</p>
+        } else {
+            return ingredients
+        }
+    }
+
     return (
         <div className={classes.Burguer}>
-            <BurguerIngredient type="bread-top"/> 
-            <BurguerIngredient type="salad"/> 
-            <BurguerIngredient type="cheese"/> 
-            <BurguerIngredient type="meat"/> 
-            <BurguerIngredient type="bread-bottom"/> 
+            <BurguerIngredient type="bread-top" />
+            {calcIngredients()}
+            <BurguerIngredient type="bread-bottom" />
         </div>
     );
 };
--- build-burguer/src/containers/BurguerBuilder/BurguerBuiler.js
+++ build-burguer/src/containers/BurguerBuilder/BurguerBuiler.js
@@ -1,15 +1,29 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/Aux'
 
 import Burguer from '../../components/Burguer/Burguer'
+import BuildControls from '../../components/Burguer/BuildControls/BuildControls'
 
 class BurguerBuilder extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            ingredients: {
+                salad: 1,
+                bacon: 1,
+                cheese: 0,
+                meat: 0
+            }
+        }
+    }
 
     render() {
         return (
             <Aux>
-                <Burguer />
-                <div>Build Controls</div>
+                <Burguer ingredients={this.state.ingredients}/>
+                <BuildControls/>
             </Aux>
         )
     }
